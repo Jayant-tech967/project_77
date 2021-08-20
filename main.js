c13ddea11b8b4ed0ba2317c5a27d522eb1b68df2
Jayant-tech967 <81297130+Jayant-tech967@users.noreply.github.com>
2021-08-20 20:12:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function getConsole(){
+    console.log("My name is Jayant.");
+    console.log("I love to play and code.");
+     console.log("Thank you");
+     console.log("20");
+     console.log("11");
+     console.log("57");
+     console.log("2");
+}
